eeb514ac6adb1270b6a8b30d30cd42740102cbb2
Tovieye Ozi <tovieye.ozi@gmail.com>
2017-11-20 15:55:17 +0100

    fix first test

--- tests/test.js
+++ tests/test.js
@@ -5,9 +5,11 @@ const {
 } = require('chai').assert;
 
 describe('words', () => {
-  it('returns and object', () => {
+  it('returns an object', () => {
     assert.isObject(myApp.words("A sentence"));
   });
-  
+  it('returns a correct object', () => {
+    assert.equal(myApp.words("olly olly in come free"),{olly: 2, in: 1, come: 1, free: 1},);
+  });
   
 });
