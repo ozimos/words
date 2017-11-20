1ab7bd4f5c816f4c7aa883b68081b0c782ecbc45
Tovieye Ozi <tovieye.ozi@gmail.com>
2017-11-20 16:00:30 +0100

    code for first test

--- tests/test.js
+++ tests/test.js
@@ -8,8 +8,5 @@ describe('words', () => {
   it('returns an object', () => {
     assert.isObject(myApp.words("A sentence"));
   });
-  it('returns a correct object', () => {
-    assert.equal(myApp.words("olly olly in come free"),{olly: 2, in: 1, come: 1, free: 1},);
-  });
   
 });
