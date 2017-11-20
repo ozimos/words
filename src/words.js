4520204e41ce10dc09c1454b8554c3eef11480b3
Tovieye Ozi <tovieye.ozi@gmail.com>
2017-11-20 16:07:08 +0100

    code for second test

--- src/words.js
+++ src/words.js
@@ -1,4 +1,15 @@
 "use strict";
 module.exports = {
-	words: (str) => {return {};}
-}
+	words: (str) => {const result = {};
+  const arr = str.split(' ');
+  arr.forEach((word) => {
+    if (result[word]) {
+      result[word] += 1;
+    } else {
+      result[word] = 1;
+    }
+    return word;
+  });
+  return result;
+}}
+	
--- tests/test.js
+++ tests/test.js
@@ -1,12 +1,14 @@
-const myApp = require ('../src/words');
+const myApp = require ('../src/words.js');
 
 const {
   assert
 } = require('chai').assert;
 
-describe('words', () => {
-  it('returns an object', () => {
+describe('words', function() {
+  it('returns an object', function() {
     assert.isObject(myApp.words("A sentence"));
   });
-  
+  it('returns a correct object', function() {
+    assert.equal(myApp.words("olly olly in come free"),{olly: 2, in: 1, come: 1, free: 1},);
+  });
 });
